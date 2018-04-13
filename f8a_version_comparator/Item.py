1f6d9aa52240b06bb7d2286939b28187f11a61ec
Geetika Batra <geetika791@gmail.com>
2018-04-13 14:17:49 +0530

    Abstract class for Integer and String Items
    Signed-off-by: Geetika Batra gbatra@redhat.com

--- f8a_version_comparator/Item.py
+++ f8a_version_comparator/Item.py
@@ -15,13 +15,13 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-"""Base class for maven version comparator tasks."""
+"""Item class acting as base class for various item types."""
 
 
 from abc import ABCMeta, abstractmethod
 
 
-class Base(metaclass=ABCMeta):
+class Item(metaclass=ABCMeta):
     """Base class for maven version comparator tasks."""
 
     @abstractmethod
