4f520f11d454c09796e7db49a2a40ec5e6f0a619
Geetika Batra <geetika791@gmail.com>
2018-04-15 23:35:33 +0530

    Fix licence from gpl to ASL
    Signed-off-by: Geetika Batra gbatra@redhat.com

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     author='Geetika Batra',
     author_email='gbatra@redhat.com',
     description='fabric8-analytics maven version comparator',
-    license='GPLv3',
+    license=' ASL 2.0',
     keywords='fabric8 analytics Maven Version Comparator',
     url='https://github.com/fabric8-analytics/fabric8-analytics-version-comparator'
 )
