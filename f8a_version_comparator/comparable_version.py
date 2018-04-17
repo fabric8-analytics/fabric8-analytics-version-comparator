46ae515c1a6d23f22f2f311faa59430976e27a80
Geetika Batra <geetika791@gmail.com>
2018-04-17 12:24:16 +0530

    Fixes input of version into stack
    Signed-off-by: Geetika Batra gbatra@redhat.com

--- f8a_version_comparator/comparable_version.py
+++ f8a_version_comparator/comparable_version.py
@@ -8,28 +8,27 @@ class ComparableVersion():
 		_start_index = 0 
 		v_index = 0
 		while (v_index<len(version)):
-			if v_index == _start_index:
-				if version[v_index] is '.' or version[v_index] is "-":
-					v_list.append(0)
-					v_index += 1
 			if version[v_index] is '.' or version[v_index] is "-":
+				if v_index == _start_index:
+					v_list.append(0)
 				v_list.append(version[v_index])
 				v_index += 1
-				continue
 			# checks if consecutive characters are also digits.
 			# If so, it takes series of digit into account
 			if version[v_index].isdigit():
+
 				in_string = version[v_index : v_index + 1]
 				length_numerical = 1
 				while True:
 					if(v_index+length_numerical >= len(version)):
 						break
 					if version[v_index + length_numerical].isdigit():
-						in_string += in_string + str(version[v_index + length_numerical])
-						v_index += length_numerical
+						in_string += str(version[v_index + length_numerical])
+						
 						length_numerical += 1
 					else:
 						break
+				v_index += length_numerical	
 				v_list.append(int(in_string))
 			else:
 				in_string = version[v_index : v_index + 1]
@@ -39,16 +38,16 @@ class ComparableVersion():
 						break
 					black_list = ['.', '-']
 					if version[v_index + length_string] not in black_list:
-						in_string += in_string + str(version[v_index + length_string])
-						v_index += length_string
+						in_string += str(version[v_index + length_string])
 						length_string += 1
 					else:
 						break
 				v_list.append(in_string)
+				v_index += length_string
 
 		return v_list
 		
 
 if __name__ == "__main__":
-	print(ComparableVersion.parse_version("1.0-alpha-1"))
+	print(ComparableVersion.parse_version("1.11.1"))
 
