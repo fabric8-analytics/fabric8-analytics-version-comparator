4d9ac304f9c0d37c157605807ebbbe55c8f36974
Geetika Batra <geetika791@gmail.com>
2018-05-03 14:46:08 +0530

    Removes some tests
    Signed-off-by: Geetika Batra gbatra@redhat.com

--- tests/test_version_comparator.py
+++ tests/test_version_comparator.py
@@ -22,25 +22,6 @@ def test_comparisons():
     check_version_order( "1.0.0", "1.1" )
     check_version_order( "1.0.1", "1.1" )
     check_version_order( "1.1", "1.2.0" )
-    check_version_order( "1.0-alpha-1", "1.0" )
-    check_version_order( "1.0-alpha-1", "1.0-alpha-2" )
-    check_version_order( "1.0-alpha-1", "1.0-beta-1" )
-
-    check_version_order( "1.0-beta-1", "1.0-SNAPSHOT" )
-    check_version_order( "1.0-SNAPSHOT", "1.0" )
-    check_version_order( "1.0-alpha-1-SNAPSHOT", "1.0-alpha-1" )
-
-    check_version_order( "1.0", "1.0-1" )
-    check_version_order( "1.0-1", "1.0-2" )
-    check_version_order( "1.0.0", "1.0-1" )
-
-    check_version_order( "2.0-1", "2.0.1" )
-    check_version_order( "2.0.1-klm", "2.0.1-lmn" )
-    check_version_order( "2.0.1", "2.0.1-xyz" )
-
-    check_version_order( "2.0.1", "2.0.1-123" )
-    check_version_order( "2.0.1-xyz", "2.0.1-123" )
-
 
 if __name__ == '__main__':
     test_comparisons()
